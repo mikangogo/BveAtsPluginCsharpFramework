389440f4a6713ad3699d39bcfde2f566c3d95297
ito <mikan750r@gmail.com>
2019-05-16 23:36:20 +0900

    Add `Processing` classes

---
+++ BveAtsPluginCsharpFramework/Processing/AtsIntegrator.cs
@@ -0,0 +1,27 @@
+﻿namespace AtsPlugin.Processing
+{
+    public class AtsIntegrator
+    {
+        private double LastU { get; set; } = 0.0f;
+        private double LastY { get; set; } = 0.0f;
+
+        public bool IsAbsolute { get; set; } = false;
+
+        public float CalculateF(float u, float deltaTime)
+        {
+            return (float)Calculate(u, deltaTime);
+        }
+
+        public double Calculate(double u, double deltaTime)
+        {
+            double nextY = LastY + deltaTime * LastU;        // おいらー法
+
+            nextY = (IsAbsolute) && (0.0f > nextY) ? 0.0f : nextY;
+
+            LastY = nextY;
+            LastU = u;
+
+            return nextY;
+        }
+	}
+}
---
+++ BveAtsPluginCsharpFramework/Processing/AtsOperationDelay.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+
+namespace AtsPlugin.Processing
+{
+    public class AtsOperationDelay
+    {
+        private double LastU { get; set; } = 0.0f;
+        private AtsIntegrator Integrator { get; set; } = new AtsIntegrator();
+
+
+        public bool IsAbsolute 
+        {
+            set
+            {
+                Integrator.IsAbsolute = value;
+            }
+            get
+            {
+                return Integrator.IsAbsolute;
+            }
+        }
+
+
+        public double U { get; set; } = 0.0;
+        public float UF
+        {
+            get
+            {
+                return (float)U;
+            }
+            set
+            {
+                U = value;
+            }
+        }
+
+        public double Y { get; private set; } = 0.0f;
+        public float YF
+        {
+            get
+            {
+                return (float)Y;
+            }
+        }
+
+        public double Tp { get; set; } = 1000.0;
+        public float TpF
+        {
+            get
+            {
+                return (float)Tp;
+            }
+            set
+            {
+                Tp = value;
+            }
+        }
+
+        public AtsOperationDelay(double initial = 0.0)
+        {
+            Reset(initial);
+        }
+
+        public void Reset(double initial)
+        {
+            LastU = initial;
+        }
+
+        public void Calculate(double deltaTime)
+        {
+            Y = LastU = Integrator.Calculate((U - LastU) * (1.0 / Tp), deltaTime);
+        }
+    }
+}
