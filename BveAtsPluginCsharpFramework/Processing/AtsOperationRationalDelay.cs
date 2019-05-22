ebd80e28df89df92838f5b8315819befb5abf4e7
ito <mikan750r@gmail.com>
2019-05-23 01:46:48 +0900

    Add `Processing` class

---
+++ BveAtsPluginCsharpFramework/Processing/AtsOperationRationalDelay.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+
+namespace AtsPlugin.Processing
+{
+    public class AtsOperationRationalDelay
+    {
+        private double LastY { get; set; } = 0.0;
+        private AtsIntegrator Integrator { get; set; } = new AtsIntegrator();
+
+        public double U { get; set; } = 0.0;
+        public float UF { get => (float)U; set => U = value; }
+        public double Y { get; private set; } = 0.0;
+        public float YF { get => (float)Y; }
+        public double Tp { get; set; } = 1000.0;
+        public float TpF { get => (float)Tp; set => Tp = value; }
+
+
+        public AtsOperationRationalDelay(double initial = 0.0)
+        {
+            Reset(initial);
+        }
+
+        public void Reset(double initial)
+        {
+            Integrator.Reset(initial, 0.0);
+            LastY = initial;
+        }
+
+        public void Calculate(double deltaTime)
+        {
+            var delta = U * (deltaTime / Tp);
+            var diff = U - Y;
+
+
+            if (diff == 0.0)
+            {
+                return;
+            }
+
+
+            if (diff > 0.0)
+            {
+                Y += delta;
+
+
+                if (Y >= U)
+                {
+                    Y = U;
+                }
+            }
+            else if (diff < 0.0)
+            {
+                Y -= delta;
+
+
+                if (Y <= U)
+                {
+                    Y = U;
+                }
+            }
+
+
+            LastY = Y;
+        }
+    }
+}
